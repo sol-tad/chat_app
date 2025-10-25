49298bcf87e9d0099874bd4310a9b38ff18a4d45
sol-tad <soltad65@gmail.com>
2025-10-25 12:02:19 +0300

    setup axios for api call and setup zustand for state managment and add tailwind,daisyUi

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatPage() {
+  return (
+    <div>
+      Chat list page
+    </div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUpPage() {
+  return (
+    <div>
+      signup page
+    </div>
+  )
+}
+
+export default SignUpPage
