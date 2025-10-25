3b2c6d6d0fa69f94608a20f4702abd3ba931584e
sol-tad <soltad65@gmail.com>
2025-10-25 10:37:18 +0300

    get contacts,chats,messages and send message api implemented

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,19 @@
-import express from "express"
+import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
-const router=express.Router()
+const router = express.Router();
+router.use(arcjetProtection,protectRoute);
 
-router.get("/send",()=>{})
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
-export default router
+export default router;
