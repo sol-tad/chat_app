e680f7923e9f60444abe8d19760110478595b260
sol-tad <soltad65@gmail.com>
2025-10-25 18:05:59 +0300

    send message ui and api integration completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 const PORT = ENV.PORT;
-app.use(express.json()); //req.body
+app.use(express.json({limit:"10mb"})); //req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser()); //req.cookie
 app.use("/api/auth", authRoutes);
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -56,7 +56,34 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
- 
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    const { authUser } = useAuthStore.getState();
 
- 
+    const tempId = `temp-${Date.now()}`;
+
+    const optimisticMessage = {
+      _id: tempId,
+      senderId: authUser._id,
+      receiverId: selectedUser._id,
+      text: messageData.text,
+      image: messageData.image,
+      createdAt: new Date().toISOString(),
+      isOptimistic: true, // flag to identify optimistic messages (optional)
+    };
+    // immidetaly update the ui by adding the message
+    set({ messages: [...messages, optimisticMessage] });
+
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: messages.concat(res.data) });
+    } catch (error) {
+      // remove optimistic message on failure
+      set({ messages: messages });
+      toast.error(error.response?.data?.message || "Something went wrong");
+    }
+  },
 }));
