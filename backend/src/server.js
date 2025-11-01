169d6f76ab725a751ad93a45fb42ef9d418539b0
sol-tad <soltad65@gmail.com>
2025-11-01 09:44:46 +0300

    configure socket.io from backend for realtime communication

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,33 +1,35 @@
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import path from "path";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-const app = express();
 const __dirname = path.resolve();
-const PORT = ENV.PORT;
-app.use(express.json({limit:"10mb"})); //req.body
+
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(cookieParser()); //req.cookie
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make read for deployment
+// make ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use((_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
   connectDB();
 });
